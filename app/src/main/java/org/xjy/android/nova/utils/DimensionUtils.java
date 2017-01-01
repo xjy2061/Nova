fd48cad66876bbd3bf03c4d3ec2043fa955009f7
xjy2061 <xjy2061@gmail.com>
2017-01-01 19:30:05 +0800

    add ColorTabLayout

--- app/src/main/java/org/xjy/android/nova/utils/DimensionUtils.java
+++ app/src/main/java/org/xjy/android/nova/utils/DimensionUtils.java
@@ -9,6 +9,8 @@ import org.xjy.android.nova.NovaApplication;
 public class DimensionUtils {
     public static final DisplayMetrics DISPLAY_METRICS = NovaApplication.getInstance().getResources().getDisplayMetrics();
     public static final float DENSITY = DISPLAY_METRICS.density;
+    public static final float SCREEN_WIDTH = DISPLAY_METRICS.widthPixels < DISPLAY_METRICS.heightPixels ? DISPLAY_METRICS.widthPixels : DISPLAY_METRICS.heightPixels;
+    public static final float SCREEN_HEIGHT = DISPLAY_METRICS.widthPixels < DISPLAY_METRICS.heightPixels ? DISPLAY_METRICS.heightPixels : DISPLAY_METRICS.widthPixels;
 
     public static int dpToPx(float dp) {
         return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, DISPLAY_METRICS) + 0.5);
