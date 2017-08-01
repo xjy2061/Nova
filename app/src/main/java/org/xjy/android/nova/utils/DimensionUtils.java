bf5640df6186a2a5179f47463eace2e7d9cf22a9
xjy2061 <xjy2061@gmail.com>
2017-08-01 12:06:12 +0800

    improve DimensionUtils

--- app/src/main/java/org/xjy/android/nova/utils/DimensionUtils.java
+++ app/src/main/java/org/xjy/android/nova/utils/DimensionUtils.java
@@ -1,16 +1,27 @@
 package org.xjy.android.nova.utils;
 
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 
 import org.xjy.android.nova.NovaApplication;
 
 public class DimensionUtils {
-    public static final DisplayMetrics DISPLAY_METRICS = NovaApplication.getInstance().getResources().getDisplayMetrics();
-    public static final float DENSITY = DISPLAY_METRICS.density;
-    public static final float SCREEN_WIDTH = DISPLAY_METRICS.widthPixels < DISPLAY_METRICS.heightPixels ? DISPLAY_METRICS.widthPixels : DISPLAY_METRICS.heightPixels;
-    public static final float SCREEN_HEIGHT = DISPLAY_METRICS.widthPixels < DISPLAY_METRICS.heightPixels ? DISPLAY_METRICS.heightPixels : DISPLAY_METRICS.widthPixels;
+    public static final DisplayMetrics DISPLAY_METRICS;
+    public static final float DENSITY;
+    public static final int SCREEN_WIDTH_PORTRAIT;
+    public static final int SCREEN_HEIGHT_PORTRAIT;
+
+    static {
+        Resources resources = NovaApplication.getInstance().getResources();
+        boolean portrait = resources.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        DISPLAY_METRICS = resources.getDisplayMetrics();
+        DENSITY = DISPLAY_METRICS.density;
+        SCREEN_WIDTH_PORTRAIT = portrait ? DISPLAY_METRICS.widthPixels : DISPLAY_METRICS.heightPixels;
+        SCREEN_HEIGHT_PORTRAIT = portrait ? DISPLAY_METRICS.heightPixels : DISPLAY_METRICS.widthPixels;
+    }
 
     public static float dpToPx(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, DISPLAY_METRICS);
