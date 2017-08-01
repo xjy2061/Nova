3c248c277da99045fe47e31b2178423286baf358
xjy2061 <xjy2061@gmail.com>
2017-08-01 10:46:17 +0800

    add formatCount method to DigitalFormatUtils

--- app/src/main/java/org/xjy/android/nova/utils/DigitalFormatUtils.java
+++ app/src/main/java/org/xjy/android/nova/utils/DigitalFormatUtils.java
@@ -1,5 +1,8 @@
 package org.xjy.android.nova.utils;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class DigitalFormatUtils {
 
     public String getLimitCount(long count, int digits) {
@@ -13,5 +16,21 @@ public class DigitalFormatUtils {
         int exp = (int) (Math.log(bytes) / Math.log(1024));
         String pre = "KMGTPE".charAt(exp - 1) + "";
         return String.format("%." + accuracy + "f%s", bytes / Math.pow(1024, exp), pre);
+    }
+
+    private String formatCount(long count) {
+        if (count < 10000) {
+            return count + "";
+        } else if (count < 10000000) {
+            DecimalFormat format = new DecimalFormat("#####.#万");
+            format.setRoundingMode(RoundingMode.FLOOR);
+            return format.format(count / 10000.0);
+        } else if (count < 100000000) {
+            return count / 10000 + "万";
+        } else {
+            DecimalFormat format = new DecimalFormat("#####.#亿");
+            format.setRoundingMode(RoundingMode.FLOOR);
+            return format.format(count / 100000000.0);
+        }
     }
 }
